954b7e06c1473fb8b792db86a081fcfa33883a8c
Nazar-bnm <nazar.bnm@gmail.com>
2016-02-22 23:24:46 +0200

    Added 15 task

---
+++ 15-image loading/js/app.js
@@ -0,0 +1,7 @@
+(function() {
+	'use strict';
+	window.onload = function() {
+        var img = document.getElementById('img');
+        img.setAttribute('src', "img/night-city.jpg");
+    };
+})();
